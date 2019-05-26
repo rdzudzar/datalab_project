270d6dc4ac91df8d1ee23c2020ec051cb81cf352
Robert Dzudzar <33647339+rdzudzar@users.noreply.github.com>
2019-05-26 16:56:18 +1000

    Fixes image query looping; Added exceptions

--- HIPASS_interactive.py
+++ HIPASS_interactive.py
@@ -401,7 +401,7 @@ from astroquery.skyview import SkyView
 # In[92]:
 
 
-SkyView.list_surveys()
+SkyView.list_surveys() #list all available image data
 #'Optical:DSS': ['DSS',
 #                  'DSS1 Blue',
 #                  'DSS1 Red',
@@ -419,12 +419,158 @@ from astroquery.vizier import Vizier
 import pylab as pl
 
 
-# In[20]:
+# In[28]:
+
+
+c = []
+for each_galaxy in HIPASS_sources:
+    
+
+    center = coordinates.SkyCoord.from_name(each_galaxy)
+    c.append(center)
+    print(center)
+
+
+# In[34]:
+
+
+# Get image from the SkyView based on the name; radius is matched to HIPASS primary beam
+images = SkyView.get_images(position=c[2], pixels=[1000,1000], survey='DSS', radius=15*u.arcmin)
+
+image = images[0]
+
+# 'imgage' is now a fits.HDUList object; the 0th entry is the image
+mywcs = wcs.WCS(image[0].header)
+
+fig = pl.figure(figsize=(8,8))
+fig.clf() # just in case one was open before
+# use astropy's wcsaxes tool to create an RA/Dec image
+ax = fig.add_axes([0.15, 0.1, 0.8, 0.8], projection=mywcs)
+ax.set_xlabel("RA")
+ax.set_ylabel("Dec")
+
+ax.imshow(image[0].data, cmap='gray_r', interpolation='none', origin='lower',
+          norm=pl.matplotlib.colors.LogNorm())
+
+
+# In[37]:
 
 
 
 for each_galaxy in HIPASS_sources:
     
+
+    center = coordinates.SkyCoord.from_name(each_galaxy)
+    
+    # Get image from the SkyView based on the name; radius is matched to HIPASS primary beam
+    images = SkyView.get_images(position=center, pixels=[1000,1000], survey='DSS', radius=15*u.arcmin)
+    
+    image = images[0]
+    
+    # 'imgage' is now a fits.HDUList object; the 0th entry is the image
+    mywcs = wcs.WCS(image[0].header)
+    
+    fig = pl.figure(figsize=(8,8))
+    fig.clf() # just in case one was open before
+    # use astropy's wcsaxes tool to create an RA/Dec image
+    ax = fig.add_axes([0.15, 0.1, 0.8, 0.8], projection=mywcs)
+    ax.set_xlabel("RA")
+    ax.set_ylabel("Dec")
+    
+    ax.imshow(image[0].data, cmap='gray_r', interpolation='none', origin='lower',
+              norm=pl.matplotlib.colors.LogNorm())
+    
+    #ax.axis([100, 200, 100, 200])
+
+
+# In[23]:
+
+
+#Example is downloading a LOT of files!!
+
+#"""
+#Example 10
+#++++++++++
+#Retrieve Hubble archival data of M83 and make a figure
+#"""
+#from astroquery.mast import Mast, Observations
+#from astropy.visualization import make_lupton_rgb, ImageNormalize
+#import matplotlib.pyplot as plt
+#import reproject
+#
+#result = Observations.query_object('M83')
+#selected_bands = result[(result['obs_collection'] == 'HST') &
+#                        (result['instrument_name'] == 'WFC3/UVIS') &
+#                        ((result['filters'] == 'F657N') |
+#                         (result['filters'] == 'F487N') |
+#                         (result['filters'] == 'F336W')) &
+#                        (result['target_name'] == 'MESSIER-083')]
+#prodlist = Observations.get_product_list(selected_bands)
+#filtered_prodlist = Observations.filter_products(prodlist)
+#
+#downloaded = Observations.download_products(filtered_prodlist)
+#
+#blue = fits.open(downloaded['Local Path'][2])
+#red = fits.open(downloaded['Local Path'][5])
+#green = fits.open(downloaded['Local Path'][8])
+#
+#target_header = red['SCI'].header
+#green_repr, _ = reproject.reproject_interp(green['SCI'], target_header)
+#blue_repr, _ = reproject.reproject_interp(blue['SCI'], target_header)
+#
+#
+#rgb_img = make_lupton_rgb(ImageNormalize(vmin=0, vmax=1)(red['SCI'].data),
+#                          ImageNormalize(vmin=0, vmax=0.3)(green_repr),
+#                          ImageNormalize(vmin=0, vmax=1)(blue_repr),
+#                          stretch=0.1,
+#                          minimum=0,
+#                         )
+#
+#plt.imshow(rgb_img, origin='lower', interpolation='none')
+
+
+# In[74]:
+
+
+from urllib.error import HTTPError
+
+
+for each_galaxy in HIPASS_sources:
+    try:
+
+        center = coordinates.SkyCoord.from_name(each_galaxy)
+        
+        # Get image from the SkyView based on the name; radius is matched to HIPASS primary beam
+        Survey = 'DSS1 Blue'
+        images = SkyView.get_images(position=center, pixels=[1000,1000], survey=Survey, radius=15*u.arcmin)
+        
+        image = images[0]
+        
+        # 'imgage' is now a fits.HDUList object; the 0th entry is the image
+        mywcs = wcs.WCS(image[0].header)
+        
+        fig = pl.figure(figsize=(8,8))
+        fig.clf() # just in case one was open before
+        # use astropy's wcsaxes tool to create an RA/Dec image
+        ax = fig.add_axes([0.15, 0.1, 0.8, 0.8], projection=mywcs)
+        ax.set_xlabel("RA")
+        ax.set_ylabel("Dec")
+        
+        ax.imshow(image[0].data, cmap='gray_r', interpolation='none', origin='lower',
+                  norm=pl.matplotlib.colors.LogNorm())
+    except HTTPError:
+        print('Image not found in the {0} filter'.format(Survey))
+        continue
+    
+    
+    #ax.axis([100, 200, 100, 200])
+
+
+# In[45]:
+
+
+for each_galaxy in HIPASS_sources:
+
 
     center = coordinates.SkyCoord.from_name(each_galaxy)
     
@@ -445,6 +591,4 @@ for each_galaxy in HIPASS_sources:
     
     ax.imshow(image[0].data, cmap='gray_r', interpolation='none', origin='lower',
               norm=pl.matplotlib.colors.LogNorm())
-    
-    #ax.axis([100, 200, 100, 200])
 
